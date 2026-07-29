14fcb1cd24dbcb06bc205149de5b3259a31f12bc
Jitesh Yadav <jituu098@gmail.com>
2026-07-30 01:30:11 +0530

    operators with type and examples added

---
+++ 01-Basics/operators.py
@@ -0,0 +1,21 @@
+a = 4
+b = 5
+
+# Arithmetic Operators
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+# Comparison Operators
+print(a > b)
+print(a == b)
+
+# Logical Operators
+print(a > 5 and b < 10)
+print(a > 20 or b < 10)
+
+# Assignment Operator
+#a = a + 5
+a += 5
+print(a)
