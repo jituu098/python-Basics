7bb31e617eab665ac63c46dd543abbd5c729675c
Jitesh Yadav <jituu098@gmail.com>
2026-07-30 01:20:36 +0530

    data types with examles added

---
+++ 01-Basics/data_types.py
@@ -0,0 +1,11 @@
+name = "Jitesh" # string
+age = 19        # Integer
+height = 5.4    #Float
+is_student = True # Boolean
+numbers = [1,2,4]  # List
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(numbers))
