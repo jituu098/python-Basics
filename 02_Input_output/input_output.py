5e3eb249537bb086e5f04e0ef15193d893a1ae13
Jitesh Yadav <jituu098@gmail.com>
2026-07-30 01:56:09 +0530

    add input and output example

---
+++ 02_Input_output/input_output.py
@@ -0,0 +1,11 @@
+# by default input take input as a strings
+name = input("Enter you name: ")
+print("Hello", name)
+
+# Integer input
+age = int(input("Enter age: "))
+print("Age: ", age)
+
+# Float input
+height = float(input("Enter height: "))
+print("Height: " , height)
