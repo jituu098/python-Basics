253d93ff99c01603bded102bdc1c610716daf756
Jitesh Yadav <jituu098@gmail.com>
2026-07-30 16:03:50 +0530

    added comments in nest if file

--- 03_Conditionals/nested_if.py
+++ 03_Conditionals/nested_if.py
@@ -12,8 +12,8 @@
 #Q:if a person have the citizensip and age >= 18 than he/she eligible to vote
 citizen = input("Do you have a citizenship?: ")
 age = int(input("Enter your age: "))
-if age >=18:
-    if(citizen == "yes"):
+if age >=18:  # if true than goes to inner if else and if false than goes directly to else part
+    if(citizen == "yes"):  # it will execute only when above if condition true
         print("Eligible to vote: ")
     else:
         print("Not eligible becaue you are not citizen: ")
