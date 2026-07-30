49c2557a7bf1deacab4d7ebbfb68c78c27d736ae
Jitesh Yadav <jituu098@gmail.com>
2026-07-30 16:23:13 +0530

    modified with comments

--- 03_Conditionals/logical_operators.py
+++ 03_Conditionals/logical_operators.py
@@ -1,9 +1,11 @@
 age = 22;
 has_id = True
 
-if age >= 18 and has_id:
+# Executes only when both the codition will be true
+if age >= 18 and has_id:  
     print("Entery allowed")
 
+#executes even of the condition wil be true
 if age < 18 or has_id:
     print("One condition is true")
 print(not has_id)
