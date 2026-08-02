a5bf7953e9ca3a6f96574e8eb0376573d865515a
Jitesh Yadav <jituu098@gmail.com>
2026-08-02 18:50:20 +0530

    loop the range function

---
+++ 04_Loops/range_function.py
@@ -0,0 +1,18 @@
+# range(stop)
+print("range(stop):")
+for i in range(5):
+    print(i)
+
+print()
+
+# range(start, stop)
+print("range(start, stop):")
+for i in range(2, 6):
+    print(i)
+
+print()
+
+# range(start, stop, step)
+print("range(start, stop, step):")
+for i in range(0, 11, 2):
+    print(i)
