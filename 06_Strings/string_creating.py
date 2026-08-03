4fcde958c6f42953ca80100fc58b77e511ba43f2
Jitesh Yadav <jituu098@gmail.com>
2026-08-03 18:40:17 +0530

    string basics example

---
+++ 06_Strings/string_creating.py
@@ -0,0 +1,14 @@
+# Creating strings in different ways
+# strings are inside the single, double and triple quote
+name = "Jitesh"
+city = 'Bangalore'
+
+# inside the triple quotes the strings will be printed same as it is written 
+# it includes lines as well as spaces
+message = """Python is an
+easy and powerful
+programming language."""
+
+print("Name:", name)
+print("City:", city)
+print("Message:\n", message)
