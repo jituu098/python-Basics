3a8815ac55b2afe21b855dfec89c852ddd2a29d4
Jitesh Yadav <jituu098@gmail.com>
2026-08-05 19:13:03 +0530

    basics of string concatination and replication

---
+++ 06_Strings/str_concat_replication.py
@@ -0,0 +1,18 @@
+first_name = "Jitesh"
+last_name = "Yadav"
+
+full_name = first_name + " " + last_name
+
+print("Full Name:", full_name)
+
+course = "Python"
+duration = "3 Months"
+
+print(course + " - " + duration)
+
+#string replication
+print(first_name *3)
+
+# interger can be combined with the string b
+age = 21
+print(first_name + " " + str(age) + ' old')
