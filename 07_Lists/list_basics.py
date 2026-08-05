f94081ed1a983a4b09f23dcf81a34730527e77b6
Jitesh Yadav <jituu098@gmail.com>
2026-08-05 19:29:53 +0530

    list declearation and accessing of list basics

---
+++ 07_Lists/list_basics.py
@@ -0,0 +1,21 @@
+# Creating different types of lists
+# list are declead inside a square bracker
+# lists are of any data type it can contain int float str etc inside a single list only
+
+fruits = ["Apple", "Banana", "Mango"]
+numbers = [10, 20, 30, 40]
+mixed = ["Jitesh", 20, 85.5, True]
+
+print("Fruits:", fruits)
+print("Numbers:", numbers)
+print("Mixed List:", mixed)
+
+
+# ascessing of list
+# items in a list can be acces by the index value
+# acessing in a list is quite similar to string
+# -1 index gives the last item in the string
+
+print("\nAccessing Elements")
+print("First Fruit:", fruits[0])
+print("Last Number:", numbers[-1])
