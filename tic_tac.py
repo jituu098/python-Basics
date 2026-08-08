353456e7ab6051991d98cdfe9a791a80b0623188
Jitesh Yadav <jituu098@gmail.com>
2026-08-08 21:52:21 +0530

    tic tac toy game using dict and loops

---
+++ tic_tac.py
@@ -0,0 +1,21 @@
+board = {"Top-l:": " ", "Top-M": " ", "Top-R": " ",
+         "Mid-l:": " ", "Mid-M": " ", "Mid-R": " ",
+         "Low-l:": " ", "Low-M": " ", "Low-R": " " }
+def print_board(board):
+    print(board["Top-l:"] + "|" + board["Top-M"] + "|" + board["Top-R"])  
+    print("-+-+-")
+    print(board["Mid-l:"] + "|" + board["Mid-M"] + "|" + board["Mid-R"])
+    print("-+-+-")
+    print(board["Low-l:"] + "|" + board["Low-M"] + "|" + board["Low-R"])
+
+print_board(board)
+turn = "X"
+for i in range(9):
+    print("Turn for " + turn + ". Move on which space?")
+    move = input()
+    board[move] = turn
+    if turn == "X":
+        turn = "O"
+    else:
+        turn = "X"
+    print_board(board)
